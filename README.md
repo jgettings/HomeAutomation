0e970ca4091c251ebadd7f3cf258e17c1c22ba98
Jennifer Gettings <jgettings@campuslabs.com>
2017-09-27 22:47:32 -0400

    Started sunrise endpoint

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # HomeAutomation
 My home automation thingies
+
+## Running locally
+
+```node app.js```
+
+## Reloading on the server
+
+```pm2 reload all```
--- app.js
+++ app.js
@@ -1,11 +1,52 @@
 var express = require('express');
+var Milight = require('node-milight-promise').MilightController;
+var commands = require('node-milight-promise').commandsV6;
+
 var app = express();
 
+// Sunrise
 app.get('/', function(req, res) {
-  res.send('Hello World');
+	res.send('Starting sunrise...')
+
+	var light = new Milight({
+	        ip: "192.168.1.9",
+	        type: 'v6'
+	    });
+
+	var bedroom = 2;
+	var red = 26;
+	var yellow = 52;
+	var totalBrightness = 100;
+	var totalTimeInMinutes = 45;
+	var pauseTime = (totalTimeInMinutes * 60 * 1000) / totalBrightness;
+
+  light.sendCommands(commands.rgbw.on(bedroom));
+
+  var color = red;
+  for (var brightness = 0; brightness <= totalBrightness; brightness++) {
+  		light.sendCommands(commands.rgbw.brightness(bedroom, brightness))
+  		light.sendCommands(commands.rgbw.hue(bedroom, color));
+  		light.pause(pauseTime);
+
+  		if (brightness % 3 == 0 && brightness < yellow) {
+  			color++;
+  		}
+  }
+
+	light.close();
 });
 
+
+
+
 var server = app.listen(3000, function() {
-  console.log('Magic is happening on port 3000');
+  console.log('Home Automation server has started on port 3000');
+
+  // var discoverBridges = require('node-milight-promise').discoverBridges;
+	// discoverBridges({
+	//     type: 'all'
+	// }).then(function (results) {
+	//     console.log(results);
+	// });
 });
 
